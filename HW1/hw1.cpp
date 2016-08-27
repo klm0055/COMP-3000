56c5b95fb70a85af33547d0db896810c357836fc
Katie L Martin <klm0055@tux051.eng.auburn.edu>
2016-08-26 23:00:11 -0500

    This is my submission for hw1.

---
+++ HW1/hw1.cpp
@@ -0,0 +1,26 @@
+//Name: 			Katie Martin
+////Auburn UserID: 		klm0055
+////filename: 			hw1.cpp
+////Development Environment: 	vim, g++ Linux 
+////Compile: 			g++ hw1.cpp -o hw1 
+////Sample usage: 		./hw1
+//
+
+
+int main(){
+//" Question 1:
+/*I believe Jack Reeves views programming as something that has not been given credit where it is due. He believes that the focus of software engineering has been misdirected. He believes that there is little engineering that goes on in software development and construction. He believes it to be more of a craft and art rather than design.*/
+
+// Question 2:
+/* I believe that there are not more rigid standards for designing code and writing software because it is an art. I believe the point he is trying to make iis that there are a lot of moving pieces when it comes to software design and not enough emphasis is being put on the validation and improvement parr of a desgin. I believe this is due to misconceptions brought on by the actual software engineers.*/
+
+// Qeustion 3:
+/* Jack Reeves prefers C++ over some other programming languages because it is object-oriented. He believes C++ will directly supoort better software design. He likes C++ because it is able to capture high level design concepts and is suitable for real world projects. C++ also allows for the expression of high level information about design components, whic will make it easier to produce the design and refine it later.*/
+
+// Question 4:
+/* I think Jack Reeves believes that this is a problem in the software engineering world. He does not thing it can be a problem that can be escaped, due to the complexity of software designs. But, he does believe there can be improvement to the current design, refine, and test strategy. With software design and devolopment you are going to make mistakes in your code during the process, so the design, refine, and test strategy is throughout the design process. While with other engineers, they have to make sure once they are done with the design that the design will more that likely work using accepted constructio techniques. Therefore, engineers have to spend a considerable amount of time validating and refining their designs. */
+
+return 0;
+
+} 
+
